a4c0434cad9203a40bf4bd5e3c22f0bfe47cbed2
= <=>
2021-04-18 22:23:50 +0700

    hihi

---
+++ README.txt
@@ -0,0 +1,5 @@
+Chạy các lệnh:
+    npm install
+    npx tsc 
+    node dist/index.js
+Server chạy ở cổng 8080
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import express from "express"
+import Knex from "knex"
+import * as bcrypt from 'bcrypt';
+
+const app = express();
+const port = 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const knex = Knex ({
+    client: "sqlite3",
+    connection: {
+      filename : "./database.db"
+    }
+});
+
+app.get('/timestamp', function (req: express.Request, res: express.Response) {
+    return res.json({error: 0, timestamp: Date.now()});
+})
+
+app.post("/registrations", function (req: express.Request, res: express.Response) {
+    bcrypt.genSalt(10, function(err: Error, salt: string) {
+        return bcrypt.hash(req.body.password, salt, function(err: Error, hash: string) {
+            return knex("users")
+            .insert({
+                username: req.body.username, 
+                displayed_name: req.body.displayed_name, 
+                hashed_password: hash
+            }).then((id) => {
+                res.json({error: 0});
+            });
+        });
+    });
+})
+
+app.get("/registrations", function (req:express.Request, res: express.Response) {
+    return knex("users").then((rows) => {
+        res.json({error: 0, data: rows});
+    });
+});
+
+app.listen(port, function () {
+    console.log('listening at port '+port);
+})
